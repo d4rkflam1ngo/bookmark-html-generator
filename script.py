cb4f9184e8b20c64ca9ecf2e129dbc841ef3b2ef
d4rkflam1ngo <38860802+d4rkflam1ngo@users.noreply.github.com>
2022-06-08 09:51:02 +0000

    Automatically get the page title

--- script.py
+++ script.py
@@ -2,50 +2,69 @@
 
 # Import required modules
 import time
+import base64
+from urllib import response
 import favicon
 import requests
-import base64
+import bs4
 
-folder_objects = ""
+folderObjects = ""
 
 # Get base64 encoded site icon.
 def getIconBase64(url):
-  icon = favicon.get(url)[-1]
-  icon_url = icon.url
-  icon_format = icon.format
+  icon = favicon.get(url)[-1] # Select the smallest icon (last item in array)
   response = requests.get(icon.url, stream=True)
+
   # Return the b64 encoded icon
   return "data:"+str(response.headers['Content-Type'])+";"+"base64,"+base64.b64encode(response.content).decode("utf-8")
-  
+
+# Get the page title for the bookmark name
+def getPageTitle(url):
+  response = requests.get(url)
+  html = bs4.BeautifulSoup(response.text, features="html.parser")
+  print (html.title.text)
+  return html.title.text.decode("utf-8")
+
 while True:
+
   # Menu
-  menu_selection = int(input("1. Create Folder\n2. Done\n> "))
-  if menu_selection == 2: 
+  menu_selection = input("1. Create Folder\n2. Done\n> ")
+  if int(menu_selection) == 2: 
     break
-  elif menu_selection == 1:
-    folder_name = input("Please enter a folder name\n> ")
-    formatted_links = ""
+  elif int(menu_selection) == 1:
+    folderName = input("Please enter a folder name\n> ")
+    formattedLinks = ""
+
     # Ask for links
     while True:
       link = {}
-      link_name = input("Enter a link name (blank to stop adding)\n> ")
-      if link_name == "":
+      linkUrl = input("Enter a link URL (Blank to stop)\n> ")
+      if linkUrl == "":
         break
-      link["name"] = link_name
-      link["url"] = input("Enter a link URL\n> ")
+      if linkUrl[:8] != "https://" or linkUrl[:7] != "https://":
+        linkUrl = "http://"+linkUrl
+      link["url"] = linkUrl
+      
+      # Try and get the page title for the bookmark name. If not just use the URL
+      try:
+        link["name"] = getPageTitle(link["url"])
+      except:
+        link["name"] = link["url"]
+
       # Try and get the b64 encoded site icon. If not use nothing (browser will cache icon if not present)
       try:
         link["icon"] = getIconBase64(link["url"])
       except: 
         link["icon"] = ""
+
       # Add link to existing lists
-      formatted_links = formatted_links + '\n\t\t\t<DT><A HREF="{}" ADD_DATE="{}" ICON="{}">{}</A>'.format(link["url"], round(time.time()), link["icon"], link["name"])
+      formattedLinks = formattedLinks + '\n\t\t\t<DT><A HREF="{}" ADD_DATE="{}" ICON="{}">{}</A>'.format(link["url"], round(time.time()), link["icon"], link["name"])
     # Create folder and add to current folders
-    folder_objects = folder_objects+'\n\t\t<DT><H3 ADD_DATE="{}" LAST_MODIFIED="{}">{}</H3>\n\t\t<DL><p>{}\n\t\t</DL><p>'.format(round(time.time()), round(time.time()), folder_name, formatted_links)
+    folderObjects = folderObjects+'\n\t\t<DT><H3 ADD_DATE="{}" LAST_MODIFIED="{}">{}</H3>\n\t\t<DL><p>{}\n\t\t</DL><p>'.format(round(time.time()), round(time.time()), folderName, formattedLinks)
     
 # Add folders to overall bookmarks object
-bookmarks_object = '<!DOCTYPE NETSCAPE-Bookmark-file-1>\n<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n<TITLE>Bookmarks</TITLE>\n<H1>Bookmarks</H1><DL><p>\n\t<DT><H3 ADD_DATE="{}" LAST_MODIFIED="{}" PERSONAL_TOOLBAR_FOLDER="true">Bookmarks</H3>\n\t<DL><p>{}\n\t</DL><p>\n</DL><p>'.format(round(time.time()), round(time.time()), folder_objects)
+bookmarksObject = '<!DOCTYPE NETSCAPE-Bookmark-file-1>\n<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n<TITLE>Bookmarks</TITLE>\n<H1>Bookmarks</H1><DL><p>\n\t<DT><H3 ADD_DATE="{}" LAST_MODIFIED="{}" PERSONAL_TOOLBAR_FOLDER="true">Bookmarks</H3>\n\t<DL><p>{}\n\t</DL><p>\n</DL><p>'.format(round(time.time()), round(time.time()), folderObjects)
   
 # Write bookmarks to file
 bookmarks_file = open("./bookmarks.html", "w")
-bookmarks_file.writelines(bookmarks_object)
+bookmarks_file.writelines(bookmarksObject)
